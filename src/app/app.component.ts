8a9bc44e88e452a84ee9f92795f97edaf9e68c8b
Jennifer Soederberg <jennifer.soderberg@chasacademy.se>
2019-02-08 19:50:53 +0100

    Add connection to yummly API and print some test data to the view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RecipesService } from './recipes.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'angular-recipe-app';
+  recipes = [];
+
+  constructor(private recipesService: RecipesService) {}
+
+  ngOnInit() {
+    this.recipesService.getRecipes().subscribe(data => {
+      this.recipes = data.matches;
+      console.log(data.matches);
+    })
+  }
+
+
+
+
+
+
 }
--- src/app/recipes.service.ts
+++ src/app/recipes.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RecipesService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getRecipes() {
+    return this.http.get<any>('http://api.yummly.com/v1/api/recipes?_app_id=268d6b88&_app_key=886b1f26a1a4d4bf74e89d2769579f90');
+  }
 }
