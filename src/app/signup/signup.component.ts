58303982bac1be7ebcde7820ef0b612d146cdb37
Jennifer Soederberg <jennifer.soderberg@chasacademy.se>
2019-06-11 22:29:16 +0200

    Add signup form to signup page

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-signup',
@@ -7,7 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  public form = {
+    email: null,
+    name: null,
+    password: null,
+    password_confirmation: null
+  };
+
+  public error = [];
+
+
+  onSubmit() {
+    this.http.post('http://recipeapp.test/api/signup', this.form).subscribe(
+      data => console.log(data),
+      error => this.handleError(error)
+    );
+  }
+
+  handleError(error) {
+    this.error = error.error.errors;
+  }
 
   ngOnInit() {
   }
