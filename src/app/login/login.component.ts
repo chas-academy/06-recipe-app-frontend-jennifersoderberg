bc619db71e393a64aac43b7e52ce4d79b1146166
Jennifer Soederberg <jennifer.soderberg@chasacademy.se>
2019-06-11 14:26:49 +0200

    Add login form to login page

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+//import { HttpClient } from 'selenium-webdriver/http';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
@@ -7,7 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  public form = {
+    email: null,
+    password: null
+  };
+
+  onSubmit() {
+    // console.log(this.form);
+    this.http.post('http://recipeapp.test/api/login', this.form).subscribe(
+      data => console.log(data),
+      error => console.log(error)
+    );
+  }
 
   ngOnInit() {
   }
