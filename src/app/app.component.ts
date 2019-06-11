33089062b6d8327943f2903487b4f236a2ee1043
Jennifer Soederberg <jennifer.soderberg@chasacademy.se>
2019-06-12 00:23:29 +0200

    Add functionality for when user click on logout button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from './Services/auth.service';
+import { Router } from '@angular/router';
+import { TokenService } from './Services/token.service';
 
 @Component({
   selector: 'app-root',
@@ -11,10 +13,19 @@ export class AppComponent implements OnInit {
   public loggedIn: boolean;
 
   constructor(
-    private Auth: AuthService
+    private Auth: AuthService,
+    private router: Router,
+    private Token: TokenService
   ) {}
 
   ngOnInit() {
     this.Auth.authStatus.subscribe(value => this.loggedIn = value);
+  }
+
+  logout(event: MouseEvent) {
+    event.preventDefault();
+    this.Token.remove();
+    this.Auth.changeAuthStatus(false);
+    this.router.navigateByUrl('/login');
   }
 }
